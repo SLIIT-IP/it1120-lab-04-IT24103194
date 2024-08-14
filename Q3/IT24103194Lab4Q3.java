b98e79ec844ca3e03a8d343f41eb2442b0fa97b4
IT24103194 <it24103194@my.sliit.lk>
2024-08-14 10:20:15 +0530

    Add file

---
+++ Q3/IT24103194Lab4Q3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class IT24103194Lab4Q3
+{ 
+ public static void main(String[] args)
+ { 
+ int num01;
+ // variable declaration
+ Scanner input=new Scanner(System.in);
+ System.out.print("Enter a number: ");
+ num01=input.nextInt();
+ //User to input value
+  
+ if (num01 > 0)
+ //positive part
+  { 
+    System.out.print("The number is :Positive");
+  }
+else if (num01 < 0)
+ //negative part
+  {
+    System.out.print("The number is :Negative");
+  }
+ else
+  {
+    System.out.println("The number is Zero");
+  }
+ }
+}
